585ebb02190069c01703c9300b170236ce101048
xiaodaxia <xiaozisheng2008@qq.com>
2023-09-05 23:13:41 +0800

    add comment

--- kuka_remote_controller.py
+++ kuka_remote_controller.py
@@ -2,7 +2,7 @@ import re
 import sys
 import time
 from collections import OrderedDict
-from typing import List, Optional
+from typing import List, Optional, Dict
 
 from loguru import logger
 
@@ -13,13 +13,30 @@ logger.add(sys.stderr, level="INFO")
 
 
 class KUKARemoteController:
-    def __init__(self, ip: str = "192.168.1.147", port: int = 7000) -> None:
+    """KUKARemoteController is a API to control KUKA robot via socket.
+    NOTE: unit of xyz position is millimeter by default;
+    and unit of rpy is angle by default.
+    """
+
+    def __init__(self, ip: str = "172.31.1.147", port: int = 7000) -> None:
+        """KUKARemoteController
+
+        Args:
+            ip (str, optional): KUKA controller IP. Defaults to "192.168.1.147".
+            port (int, optional): KUKA controller port. Defaults to 7000.
+        """
         self.c3client = C3BridgeClient(ip, port)
         if not self.c3client.can_connect:
             logger.error(f"failed to connect to KUKA controller ({ip=}, {port=})")
         self.pattern_reply_numbers = re.compile("\W(-?[\d\.]+)\W")
 
     def get_tool(self) -> Optional[List[float]]:
+        """Get robot TOOL
+
+        Returns:
+            Optional[List[float]]: Robot TOOL, eg: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], its a
+            KUKA Frame, we convert it to xyzrpy
+        """
         reply = self.c3client.read("$TOOL")
         if reply:
             return [
@@ -28,12 +45,25 @@ class KUKARemoteController:
             ]
 
     def set_tool(self, xyzrpy: List[float]) -> bool:
+        """Set Robot TOOL
+
+        Args:
+            xyzrpy (List[float]): Robot TOOL , eg: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        Returns:
+            bool: True if success, False otherwise
+        """
         vars = OrderedDict()
         vars["COM_FRAME"] = f"{{FRAME: {self.pose_to_str(xyzrpy)}}}"
         vars["COM_ACTION"] = "5"
         return self.__send_vars(vars)
 
     def get_pose(self) -> Optional[List[float]]:
+        """Get robot tip POSE
+
+        Returns:
+            Optional[List[float]]: Robot POSE in xyzrpy, eg: [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        """
         reply = self.c3client.read("$POS_ACT_MES")
         if reply:
             numbers = self.pattern_reply_numbers.findall(reply.decode("utf-8"))
@@ -45,6 +75,12 @@ class KUKARemoteController:
             return xyzrpy
 
     def get_joints(self) -> Optional[List[float]]:
+        """Get robot joint position
+
+        Returns:
+            Optional[List[float]]: Robot joint position, first 6 is robot joints,
+            then external axis joints
+        """
         reply = self.c3client.read("$AXIS_ACT_MEAS")
         if reply:
             joints = [
@@ -54,6 +90,15 @@ class KUKARemoteController:
             return joints
 
     def movej(self, q: List[float], wait: bool = True) -> bool:
+        """Move robot joint
+
+        Args:
+            q (List[float]): target joint positions
+            wait (bool, optional): blocking or not. Defaults to True.
+
+        Returns:
+            bool: True if success
+        """
         vars = OrderedDict()
         vars["COM_E6AXIS"] = f"{{E6AXIS: {self.joints_to_str(q)}}}"
         vars["COM_ACTION"] = "2"
@@ -65,6 +110,15 @@ class KUKARemoteController:
         return bool(res)
 
     def movel(self, xyzrpy: List[float], wait: bool = True) -> bool:
+        """Move robot tip to a target pose/frame
+
+        Args:
+            xyzrpy (List[float]): target pose
+            wait (bool, optional): blocking or not. Defaults to True.
+
+        Returns:
+            bool: True if success
+        """
         vars = OrderedDict()
         vars["COM_E6POS"] = f"{{E6POS: {self.pose_to_str(xyzrpy)}}}"
         vars["COM_ACTION"] = "3"
@@ -76,26 +130,62 @@ class KUKARemoteController:
         return bool(res)
 
     def get_cartesian_speed(self) -> float:
+        """get tip cartesian speed
+
+        Returns:
+            float: speed, unit is m/s
+        """
         v = self.c3client.read("$VEL.CP")
         return float(v.decode())
 
     def get_joint_speed(self) -> float:
+        """get joint speed, unit is deg/s.
+        Note that each joint is set to a same joint speed by default.
+
+        Returns:
+            float: joint speed
+        """
         v = self.c3client.read("$VEL_AXIS[1]")
         return float(v.decode())
 
-    def set_cartesian_speed(self, meter_per_second: float):
+    def set_cartesian_speed(self, meter_per_second: float) -> bool:
+        """Set robot tip cartesian speed
+
+        Args:
+            meter_per_second (float): desired cartesian speed, m/s
+
+        Returns:
+            bool: True if success
+        """
         vars = OrderedDict()
         vars["COM_VALUE1"] = f"{meter_per_second:.4f}"
         vars["COM_ACTION"] = "6"
         return self.__send_vars(vars)
 
-    def set_joint_speed(self, deg_per_second: float):
+    def set_joint_speed(self, deg_per_second: float) -> bool:
+        """Set robot joints to the given speed
+
+        Args:
+            deg_per_second (float): desired speed, deg/s
+
+        Returns:
+            bool: True if success
+        """
         vars = OrderedDict()
         vars["COM_VALUE2"] = f"{deg_per_second:.4f}"
         vars["COM_ACTION"] = "7"
         return self.__send_vars(vars)
 
-    def set_digital_output(self, do_id: int, do_val: bool):
+    def set_digital_output(self, do_id: int, do_val: bool) -> bool:
+        """Set robot digital output
+
+        Args:
+            do_id (int): digital output id
+            do_val (bool): digital output value
+
+        Returns:
+            bool: True if success
+        """
         vars = OrderedDict()
         vars["COM_VALUE1"] = f"{do_id}"
         vars["COM_VALUE2"] = f"{int(do_val)}"
@@ -135,7 +225,15 @@ class KUKARemoteController:
             qstr += f", E6 {q[11]:.3f}"
         return qstr
 
-    def __send_vars(self, vars) -> bool:
+    def __send_vars(self, vars: Dict[str, str]) -> bool:
+        """Write variables to KUKA controller
+
+        Args:
+            vars (Dict[str, str]): variable names and values
+
+        Returns:
+            bool: True if success
+        """
         res = None
         for k, v in vars.items():
             res = self.c3client.write(k, v)
@@ -145,6 +243,7 @@ class KUKARemoteController:
         return res is not None
 
     def __wait_motion(self):
+        """Blocking until robot motion finished"""
         logger.info("waiting task to be finished")
         while True:
             if self.c3client.read("COM_ACTION") == b"0":
